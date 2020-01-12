a81981e16876463c85d0daa04bcbec023b07d983
Damon Allison <damon@branchapp.com>
2020-01-11 20:02:25 -0600

    Unit testing

---
+++ src/app.Tests/LanguageTests.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+
+namespace app.Tests
+{
+    enum Color {
+        RED,
+        BLUE
+    }
+    public class LanguageTests
+    {
+        [Fact]
+        public void Switch()
+        {
+        }
+    }
+}
